f672df9efea4aae58c9b15a6f6dfa51df1af5e47
Arkadipta-Kundu <arkadiptak49@gmail.com>
2025-07-23 11:24:42 +0530

    solved LC 875 koko eating bananas

--- 875.koko-eating-bananas.java
+++ 875.koko-eating-bananas.java
@@ -6,9 +6,39 @@
 
 // @lc code=start
 class Solution {
+    public int calMax(int[] piles) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > max) {
+                max = piles[i];
+            }
+        }
+        return max;
+    }
+
+    public long calEatingTime(int[] piles, int k) {
+        long totalTime = 0;
+        for (int i = 0; i < piles.length; i++) {
+            totalTime += (piles[i] + (k - 1L)) / k;
+        }
+        return totalTime;
+    }
+
     public int minEatingSpeed(int[] piles, int h) {
-        
+        int low = 1;
+        int high = calMax(piles);
+        int ans = high;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long totalEatingTime = calEatingTime(piles, mid);
+            if (totalEatingTime <= h) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 }
 // @lc code=end
-
